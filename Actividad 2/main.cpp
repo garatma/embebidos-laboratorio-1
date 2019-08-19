21e0499a30e8ae15ce3beb9f88efa9102922843a
Manuel Garat <m.garat@outlook.com>
2019-08-19 00:02:50 -0300

    Actividad 2 simulada con variable alternate en cada iteración

--- Actividad 2/main.cpp
+++ Actividad 2/main.cpp
@@ -8,12 +8,12 @@ int buttonState = 0;
 int main()
 {
 	//Inicializar el pin del pulsador como entrada
-	DDRD &= ~(1<<DDD2);
+	DDRD |= (1<<DDD2);
 	//Inicializar el pin del led como salida
 	DDRB |= (1<<DDB5);
 
 	while (1)
-	{
+	{		
 		//Leer el estado del pulsador
 		buttonState = PIND & (1<<PD2);
 
@@ -22,11 +22,13 @@ int main()
 			//Si el pulsador está apretado
 			//Encender el led
 			PORTB |= (1<<PB5);
+			PORTD &= ~(1<<PD2);
 		}
 		else
 		{
 			//Apagar el led
 			PORTB &= ~(1<<PB5);
+			PORTD |= (1<<PD2);
 		}
 
 	}
